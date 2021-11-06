d242e9ef3238b54fb19f14d3cd823cc7d5bca1da
Alexey Markov <pepelazz00@gmail.com>
2021-11-06 20:12:41 +0300

    Update main.go

--- projectTemplate/main.go
+++ projectTemplate/main.go
@@ -39,9 +39,9 @@ func getProject() t.ProjectType {
 	// боковое меню для Vue
 	p.Vue.Menu = []t.VueMenu{
 		//{DocName: "client_order"},
-		{Url: "users", Text: "Пользователи", Icon: "https://image.flaticon.com/icons/svg/423/423063.svg", Roles: []string{utils.RoleAdmin}},
+		{Url: "users", Text: "Пользователи", Icon: "image/users.svg", Roles: []string{utils.RoleAdmin}},
 		{DocName: "legal_entity"},
-		{Text: "Справочники", Icon: "https://image.flaticon.com/icons/svg/1643/1643260.svg", IsFolder: true, LinkList: []t.VueMenu{{DocName: "legal_entity"}}},
+		{Text: "Справочники", Icon: "image/directory.png", IsFolder: true, LinkList: []t.VueMenu{{DocName: "legal_entity"}}},
 	}
 	p.FillSideMenu()
 
